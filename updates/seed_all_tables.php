80a8da309604e273e77e473604899aff4b319cc6
Szabó Gergő <gergo85@gmail.com>
2015-01-26 08:37:15 +0100

    Added HU states

--- updates/seed_all_tables.php
+++ updates/seed_all_tables.php
@@ -516,6 +516,30 @@ class SeedAllTables extends Seeder
             ["name"=>"Yorkshire",         "code"=>"YOR"]
         ]);
 
+        $hu = Country::whereCode('HU')->first();
+        $hu->states()->createMany([
+            ['code' => 'BUD', 'name' => 'Budapest'],
+            ['code' => 'BAR', 'name' => 'Baranya'],
+            ['code' => 'BKM', 'name' => 'Bács-Kiskun'],
+            ['code' => 'BEK', 'name' => 'Békés'],
+            ['code' => 'BAZ', 'name' => 'Borsod-Abaúj-Zemplén'],
+            ['code' => 'CSO', 'name' => 'Csongrád'],
+            ['code' => 'FEJ', 'name' => 'Fejér'],
+            ['code' => 'GMS', 'name' => 'Győr-Moson-Sopron'],
+            ['code' => 'HBM', 'name' => 'Hajdú-Bihar'],
+            ['code' => 'HEV', 'name' => 'Heves'],
+            ['code' => 'JNS', 'name' => 'Jász-Nagykun-Szolnok'],
+            ['code' => 'KEM', 'name' => 'Komárom-Esztergom'],
+            ['code' => 'NOG', 'name' => 'Nógrád'],
+            ['code' => 'PES', 'name' => 'Pest'],
+            ['code' => 'SOM', 'name' => 'Somogy'],
+            ['code' => 'SSB', 'name' => 'Szabolcs-Szatmár-Bereg'],
+            ['code' => 'TOL', 'name' => 'Tolna'],
+            ['code' => 'VAS', 'name' => 'Vas'],
+            ['code' => 'VES', 'name' => 'Veszprém'],
+            ['code' => 'ZAL', 'name' => 'Zala']
+        ]);
+
     }
 
 }
