00c37d93095289eedaceae37534bf80fa52bef6b
reg2005 <cmp08@ya.ru>
2016-07-07 01:37:16 +0300

    Added rules logic, and soft implement Behavior Translatable for rule_text

--- models/Throttle.php
+++ models/Throttle.php
@@ -15,4 +15,22 @@ class Throttle extends ThrottleBase
     public $belongsTo = [
         'user' => ['RainLab\User\Models\User']
     ];
+
+    /**
+     * Check user throttle status.
+     * @return bool
+     * @throws AuthException
+     */
+    public function check()
+    {
+        if ($this->is_banned) {
+            throw new AuthException(sprintf(Lang::get('rainlab.user::lang.login.user_banned'), $this->user->getLogin()));
+        }
+
+        if ($this->checkSuspended()) {
+            throw new AuthException(sprintf(Lang::get('rainlab.user::lang.login.user_suspended'), $this->user->getLogin()));
+        }
+
+        return true;
+    }
 }
