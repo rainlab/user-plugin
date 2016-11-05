084ebd35d7084b11c81590e6c25d9e2ca82685ed
kamel <cracki@gmail.com>
2016-11-05 17:01:09 +0330

    Update lang.php
    Translate one missed item

--- lang/fa/lang.php
+++ lang/fa/lang.php
@@ -106,7 +106,7 @@
         'reset_password_comment' => 'جهت تنظیم مجدد کلمه عبور کاربر ، کلمه عبور جدید را وارد نمایید.',
         'confirm_password' => 'تایید کلمه عبور',
         'confirm_password_comment' => 'جهت تایید کلمه عبور را مجددا وارد نمایید.',
-        'groups' => 'Groups',
+        'groups' => 'گرو ها',
         'empty_groups' => 'گروه کاربری موجود نیست',
         'avatar' => 'نمایه',
         'details' => 'جزییات',
