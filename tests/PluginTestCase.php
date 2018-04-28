565a3a8555034b02fdad116fe32b59c5020a9b1c
scottbedard <scottbedard90@gmail.com>
2018-04-28 10:24:59 -0700

    begin scaffolding test suite

---
+++ tests/PluginTestCase.php
@@ -0,0 +1,38 @@
+<?php namespace RainLab\User\Tests;
+
+use App;
+use Artisan;
+use Illuminate\Foundation\AliasLoader;
+use PluginTestCase as BasePluginTestCase;
+use RainLab\User\Models\Settings;
+
+abstract class PluginTestCase extends BasePluginTestCase
+{
+    /**
+     * @var array   Plugins to refresh between tests.
+     */
+    protected $refreshPlugins = [
+        'RainLab.User',
+    ];
+
+    /**
+     * Perform test case set up.
+     * 
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        // reset any modified settings
+        Settings::resetDefault();
+
+        // // register the auth facade
+        // $alias = AliasLoader::getInstance();
+        // $alias->alias('Auth', 'RainLab\User\Facades\Auth');
+    
+        // App::singleton('user.auth', function() {
+        //     return \RainLab\User\Classes\AuthManager::instance();
+        // });
+    }
+}
