6b05a129f90980661cbb6dc535bdb6d597705cfa
Samuel Georges <sam@daftspunk.com>
2025-11-29 15:37:01 +1100

    Fixes column definition in user tailor field

--- contentfields/usersfield/partials/_column_multi.php
+++ contentfields/usersfield/partials/_column_multi.php
@@ -1,1 +1,10 @@
-
+<?php if ($value): ?>
+    <ul class="list-link-list">
+        <?php foreach ($value as $entry): ?>
+            <?php
+                $url = Backend::url('user/users/preview/'.$entry->id);
+            ?>
+            <li><a href="<?= $url ?>"><?= e($entry->full_name) ?></a></li>
+        <?php endforeach ?>
+    </ul>
+<?php endif ?>
--- contentfields/usersfield/partials/_column_single.php
+++ contentfields/usersfield/partials/_column_single.php
@@ -1,1 +1,8 @@
-
+<?php if ($value): ?>
+    <ul class="list-link-list">
+        <?php
+            $url = Backend::url('user/users/preview/'.$value->id);
+        ?>
+        <li><a href="<?= $url ?>"><?= e($value->full_name) ?></a></li>
+    </ul>
+<?php endif ?>
